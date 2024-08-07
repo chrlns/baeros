e76890075b1e9077b6f70f1e444e4ff176ad3607
Christian Lins <christian@lins.me>
2024-08-07 09:57:58 +0200

    Sometimes not working heap seems to be fixed (put heap in bss segment)

--- nucleus/src/main.s
+++ nucleus/src/main.s
@@ -4,10 +4,7 @@ _start:     # define entrypoint to avoid warning
 # We start in supervisor mode (svc32)
 
 # Disable interrupts
-#cpsid i
-
-# Initialize stack
-ldr sp, =stack_top
+cpsid i
 
 # Set SCTLR.U = 1 to disable legacy ARMv5 unaligned-word-behaviour
 # See https://gitlab.com/qemu-project/qemu/-/issues/1878
@@ -15,6 +12,9 @@ mrc	p15, 0, r0, c1, c0, 0   @ read SCTLR
 bic	r0, r0, #2              @ A (no unaligned access fault)
 orr	r0, #1 << 22            @ U (v6 unaligned access model)
 mcr	p15, 0, r0, c1, c0, 0   @ write SCTLR
+
+# Initialize stack
+ldr sp, =stack_top
 
 # Call nucleus (kernel) main
 b nuc_main
@@ -26,7 +26,7 @@ stack_top:
     .space 1024
 
 # Some space for a simple and very small heap
-.section data
+.section bss
 .align 4
 .globl small_heap
 small_heap:
